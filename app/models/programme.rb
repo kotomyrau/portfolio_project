d0a1c6664283626fdcb92d8112949c9aab9e3aa4
Kotomyra Uchida <kotomyrau@gmail.com>
2021-01-04 22:19:48 +0000

    added model associations

--- app/models/programme.rb
+++ app/models/programme.rb
@@ -1,2 +1,4 @@
 class Programme < ApplicationRecord
+  has_many :project_programmes
+  has_many :projects, through: :project_programmes
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,4 @@
 class Project < ApplicationRecord
+  has_many :project_programmes
+  has_many :programmes, through: :project_programmes
 end
