062c2e74cfcfdb758b4161cb60f23842cbc7b319
Kotomyra Uchida <kotomyrau@gmail.com>
2021-01-04 23:26:20 +0000

    projects new/create

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,28 @@
 class ProjectsController < ApplicationController
+
+  def new
+    @project = Project.new
+    @user = current_user
+
+  end
+
+  def create
+    @user = current_user
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to project_path(@project), notice: 'Your project has been created!'
+    else
+      render :new
+    end
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :description, :project_date, :contributors, :link)
+  end
 end
