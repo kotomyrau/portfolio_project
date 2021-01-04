1bf63cdfe55bc3018557c7ffa77bfeeeb3f697bc
Kotomyra Uchida <kotomyrau@gmail.com>
2021-01-04 22:33:05 +0000

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'about', to: 'pages#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :projects, only: [:index, :show]
+  resources :programmes, only: [:index, :show]
+
+  resources :users, only: :show do
+    resources :projects, except: [:index, :show]
+    resources :programmes, except: [:index, :show]
+  end
 end
