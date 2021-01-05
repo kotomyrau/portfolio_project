2071215d0768766ae7400d539da5caa18e8f45b6
Kotomyra Uchida <kotomyrau@gmail.com>
2021-01-05 00:08:39 +0000

    added forms

--- app/controllers/programmes_controller.rb
+++ app/controllers/programmes_controller.rb
@@ -1,2 +1,21 @@
 class ProgrammesController < ApplicationController
+  def new
+    @programme = Programme.new
+        @user = current_user
+
+  end
+
+  def create
+    @programme = Programme.new
+    @user = current_user
+    if @programme.save
+      redirect_to programme_path(@programme)
+    else
+      render :new
+    end
+  end
+
+    def show
+      @programme = Programme.find(params[:id])
+    end
 end
