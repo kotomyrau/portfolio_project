0e8c99ede718db81bb890d9b98d47f0733117516
Kotomyra Uchida <kotomyrau@gmail.com>
2021-01-06 18:13:19 +0000

    added programmes used to projects

--- app/controllers/project_programmes_controller.rb
+++ app/controllers/project_programmes_controller.rb
@@ -6,13 +6,13 @@ class ProjectProgrammesController < ApplicationController
     @projects = Project.all
     @project = Project.find(params[:project_id])
     @programmes = Programme.all
+
   end
 
   def create
     @user = current_user
     @projects = Project.all
     # @project = Project.find(params[:id])
-    @programmes = Programme.all
     @project_programme = ProjectProgramme.new(project_programme_params)
     if @project_programme.save
       redirect_to projects_path
