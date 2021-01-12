4d126ad64d9d0babb3f8b76cb26e8a280541fdbc
Kotomyra Uchida <kotomyrau@gmail.com>
2021-01-12 17:43:00 +0000

    starting project show css

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,6 @@ class PagesController < ApplicationController
   def home
     @projects = Project.all
     @preview_projects = @projects.last(3)
-    # @programmes = @preview_projects.programmes
     @project = @projects[1]
   end
 
