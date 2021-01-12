95626ab58434d48a9d40d696b87fe0ecb03e8a61
Kotomyra Uchida <kotomyrau@gmail.com>
2021-01-12 11:33:13 +0000

    started grid css

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   def home
     @projects = Project.all
     @preview_projects = @projects.last(3)
+    @programmes = @preview_projects.programmes
     @project = @projects[1]
   end
 
