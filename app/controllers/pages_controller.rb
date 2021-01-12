5dda1b5fbe5882cfb32faae7f0e8f1e6b3fb6b51
Kotomyra Uchida <kotomyrau@gmail.com>
2021-01-12 12:55:22 +0000

    added preview covers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   def home
     @projects = Project.all
     @preview_projects = @projects.last(3)
-    @programmes = @preview_projects.programmes
+    # @programmes = @preview_projects.programmes
     @project = @projects[1]
   end
 
