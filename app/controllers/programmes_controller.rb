021f02955306d8da606b797f143585e2c00001b2
Kotomyra Uchida <kotomyrau@gmail.com>
2021-01-25 11:10:29 +0000

    added new card

--- app/controllers/programmes_controller.rb
+++ app/controllers/programmes_controller.rb
@@ -17,7 +17,6 @@ class ProgrammesController < ApplicationController
     def show
       @programme = Programme.find(params[:id])
       @project_programme = ProjectProgramme.where(programme_id: @programme.id)
-
     end
 
 
