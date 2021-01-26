846f5a64cd71998ae4076ebf19bcc275a66dec52
Kotomyra Uchida <kotomyrau@gmail.com>
2021-01-26 09:04:45 +0000

    added route names

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
   # devise_for :users
-  devise_for :users, controllers: { registrations: "users/registrations" }
+  devise_for :users, :path => 'account', controllers: { registrations: "users/registrations"}
   root to: 'pages#home'
   get 'about', to: 'pages#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :projects, only: [:index, :show]
-  resources :programmes, only: [:show]
+  resources :projects, :path => 'portfolio', only: [:index, :show]
+  resources :programmes, :path => 'skills', only: [:show]
 
   resources :users, :path => 'account', only: :index do
-    resources :projects, except: [:index, :show] do
-      resources :project_programmes
+    resources :projects, :path => 'portfolio', except: [:index, :show] do
+      resources :project_programmes, :path => 'project_skills', only: [:new, :create]
     end
-    resources :programmes, except: [:index, :show]
-
+    resources :programmes, :path => 'skills', except: [:index, :show]
   end
 end
