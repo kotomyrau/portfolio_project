5e0f439df03deab23d48f34f0f64e02d62b91b68
Koto Uchida <kotomyrau@gmail.com>
2025-06-30 02:51:47 +0900

    Clean up files

--- app/controllers/programmes_controller.rb
+++ app/controllers/programmes_controller.rb
@@ -1,38 +1,6 @@
 class ProgrammesController < ApplicationController
-  def new
-    @programme = Programme.new
-    @user = current_user
-  end
-
-  def create
-    @programme = Programme.new(programme_params)
-    @user = current_user
-    if @programme.save
-      redirect_to users_path
-    else
-      render :new
-    end
-  end
-
     def show
       @programme = Programme.find(params[:id])
       @project_programme = ProjectProgramme.where(programme_id: @programme.id)
     end
-
-
-
-    def destroy
-      @programme = Programme.find(params[:id])
-      if @programme.destroy
-        flash[:notice] = "#{@programme.name} has been deleted"
-        redirect_to root_path
-      else
-        redirect_to programme_path(@programme), notice: 'There was a problem, this programme was not deleted!'
-      end
-    end
-  private
-
-  def programme_params
-    params.require(:programme).permit(:name, :skill)
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one_attached :avatar, dependent: :destroy
-
   enum :role, { user: 0, admin: 1 }, prefix: true
 end
